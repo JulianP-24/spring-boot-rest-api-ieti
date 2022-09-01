c7cb9914913429ee8544113c8d7be90b0010e8c7
Julian Peña <julian.pena-m@mail.escuelaing.edu.co>
2022-09-01 15:31:31 -0500

    Agregando Readme y License

--- src/main/java/edu/escuelaing/ieti/lab2/controller/UserController.java
+++ src/main/java/edu/escuelaing/ieti/lab2/controller/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
